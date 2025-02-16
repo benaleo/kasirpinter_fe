414d3ed1db3684f49b1b0de8d89370877dd166db
benaleo <benaleobayu@gmail.com>
2025-02-17 05:07:05 +0700

    add edit and delete category

--- lib/services/ms_product_category_service.dart
+++ lib/services/ms_product_category_service.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:kasirpinter_fe/components/data_model.dart';
 
 import 'auth_service.dart';
 
 class MsProductCategoryService {
   final String _baseUrl = dotenv.get('BASE_URL');
   final String _mainUrl = 'cms/v1/product_category';
+  final String _mainAppUrl = 'api/v1/ms/product-category';
 
   Future<Map<String, dynamic>> fetchProductCategories(
       {int page = 0,
@@ -21,7 +23,7 @@ class MsProductCategoryService {
     }
 
     String url =
-        '$_baseUrl/$_mainUrl?pages=$page&limit=$limit&sortBy=$sortBy&direction=$direction&keyword=$keyword';
+        '$_baseUrl/$_mainAppUrl?pages=$page&limit=$limit&sortBy=$sortBy&direction=$direction&keyword=$keyword';
     final response = await http.get(
       Uri.parse(url),
       headers: {
@@ -39,32 +41,97 @@ class MsProductCategoryService {
     }
   }
 
-  Future<void> createProductCategory({required String name}) async {
+  Future<void> createUpdateProductCategory(
+      {String? dataId,
+      required String name,
+      required String type,
+      bool? isActive}) async {
     final AuthService authService = AuthService();
     final String? token = await authService.getToken();
     if (token == null) {
       throw Exception("Token not found");
     }
 
-    String url = '$_baseUrl/$_mainUrl';
+    String createUrl = '$_baseUrl/$_mainUrl';
+    String updateUrl = '$_baseUrl/$_mainUrl/$dataId';
     final response = await http.post(
-      Uri.parse(url),
+      Uri.parse(dataId == null ? createUrl : updateUrl),
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer $token',
       },
       body: json.encode({
         'name': name,
-        'isActive': true,
+        'type': type,
+        'isActive': dataId == null ? true : isActive,
       }),
     );
 
-    print("url is: $url");
+    print("url is: $createUrl");
     print("Response status: ${response.statusCode}");
     print("Response body: ${response.body}"); // Add this line
 
     if (response.statusCode != 201) {
       throw Exception('Failed to create product category');
+    }
+  }
+
+  Future<ApiResponse> deleteProductCategory(String productId) async {
+    final AuthService authService = AuthService();
+    final String? token = await authService.getToken();
+    if (token == null) {
+      throw Exception("Token not found");
+    }
+
+    String url = '$_baseUrl/$_mainUrl/$productId/delete';
+    final response = await http.put(
+      Uri.parse(url),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'accept': '*/*',
+      },
+    );
+
+    print("url is : $url");
+    print("token is : $token");
+    final Map<String, dynamic> responseData = json.decode(response.body);
+    print("Response data: $responseData");
+    print("Response body: ${response.body}");
+
+    if (responseData['success']) {
+      print('Successfully deleted product');
+      return ApiResponse(
+          success: responseData['success'],
+          message: responseData['message'],
+          data: responseData['data']);
+    } else {
+      print('Failed to delete product');
+      return ApiResponse(success: false, message: 'Failed to delete product');
+    }
+  }
+
+  Future<Map<String, dynamic>> getDetailProductCategory(
+      String productId) async {
+    final AuthService authService = AuthService();
+    final String? token = await authService.getToken();
+    if (token == null) {
+      throw Exception("Token not found");
+    }
+
+    String url = '$_baseUrl/$_mainUrl/$productId';
+    final response = await http.get(
+      Uri.parse(url),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'accept': '*/*',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body)['data'];
+      return data;
+    } else {
+      throw Exception('Failed to load product details');
     }
   }
 }
--- lib/tab/ms_product_category_tab.dart
+++ lib/tab/ms_product_category_tab.dart
@@ -17,19 +17,23 @@ class _MsProductCategoryTabState extends State<MsProductCategoryTab> {
   String _searchKeyword = '';
   Timer? _debounce;
   bool _isLoading = true;
+  String name = '';
+  bool isActive = true;
 
   Future<void> _fetchCategories() async {
     setState(() {
       _isLoading = true;
     });
     try {
-      final response = await _service.fetchProductCategories(page: _currentPage, keyword: _searchKeyword);
+      final response = await _service.fetchProductCategories(
+          page: _currentPage, keyword: _searchKeyword);
       setState(() {
-        _categories = List<Map<String, dynamic>>.from(response['data']['result']);
+        _categories =
+            List<Map<String, dynamic>>.from(response['data']['result']);
       });
     } catch (e) {
       print('Error fetching categories: $e');
-    }finally{
+    } finally {
       setState(() {
         _isLoading = false;
       });
@@ -46,7 +50,7 @@ class _MsProductCategoryTabState extends State<MsProductCategoryTab> {
     });
   }
 
-  void _showAddCategoryDialog() {
+  void _showAddCategoryDialog(String? dataId, String? _name, bool? _isActive) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -67,23 +71,53 @@ class _MsProductCategoryTabState extends State<MsProductCategoryTab> {
                   textAlign: TextAlign.center,
                 ),
                 SizedBox(height: 20.0),
-                Poppins(
-                  text: "Nama :",
-                  size: 16.0,
+
+                // name
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Poppins(text: "Nama :", size: 16.0),
+                    SizedBox(height: 10.0),
+                    TextField(
+                      controller: TextEditingController(text: _name),
+                      onChanged: (value) {
+                        _name = value;
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(color: Colors.grey[200]!),
+                        ),
+                        hintText: 'Masukan nama kategori',
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(height: 10.0),
-                TextField(
-                  onChanged: (value) {
-                    name = value;
-                  },
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide(color: Colors.grey[200]!),
-                    ),
-                    hintText: 'Masukan nama kategori',
-                  ),
-                ),
+
+                // isActive
+                if (dataId != null)
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Poppins(text: "Status :", size: 16.0),
+                      SizedBox(height: 10.0),
+                      DropdownButtonFormField<bool>(
+                        decoration: InputDecoration(
+                          hintText: 'Status apakah aktif?',
+                          border: OutlineInputBorder(),
+                        ),
+                        value: _isActive,
+                        items: [
+                          DropdownMenuItem(value: true, child: Text('Aktif')),
+                          DropdownMenuItem(
+                              value: false, child: Text('Non Aktif')),
+                        ],
+                        onChanged: (value) =>
+                            setState(() => _isActive = value!),
+                      ),
+                    ],
+                  )
               ],
             ),
           ),
@@ -107,7 +141,11 @@ class _MsProductCategoryTabState extends State<MsProductCategoryTab> {
               bgColor: Colors.blue,
               color: Colors.white,
               onPressed: () async {
-                await _service.createProductCategory(name: name);
+                await _service.createUpdateProductCategory(
+                    dataId: dataId,
+                    name: name,
+                    type: 'MENU',
+                    isActive: isActive);
                 // close popup
                 Navigator.of(context).pop();
                 // add toast
@@ -125,6 +163,66 @@ class _MsProductCategoryTabState extends State<MsProductCategoryTab> {
               },
             )
           ],
+        );
+      },
+    );
+  }
+
+  void _popupDeleteProductCategory(category) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return ConfirmDialog(
+          onPressed: () async {
+            setState(() {
+              _isLoading = true;
+            });
+            try {
+              final MsProductCategoryService service =
+                  MsProductCategoryService();
+              final response =
+                  await service.deleteProductCategory(category['id']);
+              print('Response after delete: $response');
+              if (response.success) {
+                Fluttertoast.showToast(
+                  msg: "Berhasil menghapus kategori!",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.BOTTOM,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.green,
+                  textColor: Colors.white,
+                  fontSize: 16.0,
+                );
+                _fetchCategories();
+              } else {
+                Fluttertoast.showToast(
+                  msg: "Gagal menghapus kategori!",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.BOTTOM,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.red,
+                  textColor: Colors.white,
+                  fontSize: 16.0,
+                );
+              }
+            } catch (e) {
+              print('Failed to delete kategori: $e');
+              Fluttertoast.showToast(
+                msg: "Gagal menghapus kategori!",
+                toastLength: Toast.LENGTH_SHORT,
+                gravity: ToastGravity.BOTTOM,
+                timeInSecForIosWeb: 1,
+                backgroundColor: Colors.red,
+                textColor: Colors.white,
+                fontSize: 16.0,
+              );
+            } finally {
+              setState(() {
+                _isLoading = false;
+              });
+            }
+          },
+          text: "Apakah kamu yakin akan menghapus produk ini?",
         );
       },
     );
@@ -171,16 +269,22 @@ class _MsProductCategoryTabState extends State<MsProductCategoryTab> {
           ),
         ),
         drawer: DrawerElement(),
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.grey[200],
         body: Container(
+          margin: EdgeInsets.all(20.0),
           padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  PoppinsBold(text: "Daftar Kategori", size: 24.0, color: Colors.black),
+                  PoppinsBold(
+                      text: "Daftar Kategori", size: 24.0, color: Colors.black),
                   ElevatedButtonCustom(
                     text: "Tambah",
                     size: 16.0,
@@ -188,7 +292,7 @@ class _MsProductCategoryTabState extends State<MsProductCategoryTab> {
                     bgColor: Colors.blue,
                     color: Colors.white,
                     boxHeight: 40.0,
-                    onPressed: _showAddCategoryDialog,
+                    onPressed: () => _showAddCategoryDialog(null, "", true),
                   ),
                 ],
               ),
@@ -217,15 +321,34 @@ class _MsProductCategoryTabState extends State<MsProductCategoryTab> {
                   child: Container(
                     width: widthDevice,
                     child: DataTable(
-                      headingRowColor: MaterialStateColor.resolveWith((states) => Color(0xFF464646)),
+                      headingRowColor: MaterialStateColor.resolveWith(
+                          (states) => Color(0xFF464646)),
                       columnSpacing: 20.0,
                       columns: [
-                        DataColumn(label: PoppinsBold(text: 'ID', size: 16.0, color: Colors.white)),
-                        DataColumn(label: PoppinsBold(text: 'Name', size: 16.0, color: Colors.white)),
-                        DataColumn(label: PoppinsBold(text: 'Status', size: 16.0, color: Colors.white)),
-                        DataColumn(label: PoppinsBold(text: 'Type', size: 16.0, color: Colors.white)),
-                        DataColumn(label: PoppinsBold(text: 'Total Data', size: 16.0, color: Colors.white)),
-                        DataColumn(label: PoppinsBold(text: 'Action', size: 16.0, color: Colors.white)),
+                        DataColumn(
+                            label: PoppinsBold(
+                                text: 'ID', size: 16.0, color: Colors.white)),
+                        DataColumn(
+                            label: PoppinsBold(
+                                text: 'Name', size: 16.0, color: Colors.white)),
+                        DataColumn(
+                            label: PoppinsBold(
+                                text: 'Status',
+                                size: 16.0,
+                                color: Colors.white)),
+                        DataColumn(
+                            label: PoppinsBold(
+                                text: 'Type', size: 16.0, color: Colors.white)),
+                        DataColumn(
+                            label: PoppinsBold(
+                                text: 'Total Data',
+                                size: 16.0,
+                                color: Colors.white)),
+                        DataColumn(
+                            label: PoppinsBold(
+                                text: 'Action',
+                                size: 16.0,
+                                color: Colors.white)),
                       ],
                       rows: List<DataRow>.generate(
                         _isLoading ? 5 : _categories.length,
@@ -243,28 +366,45 @@ class _MsProductCategoryTabState extends State<MsProductCategoryTab> {
                             final category = _categories[index];
                             return DataRow(
                               color: MaterialStateColor.resolveWith(
-                                (states) => index.isOdd ? Colors.grey.shade50 : Colors.white,
+                                (states) => index.isOdd
+                                    ? Colors.grey.shade50
+                                    : Colors.white,
                               ),
                               cells: [
-                                DataCell(Poppins(text: (index + 1 + _currentPage * 10).toString(), size: 14)),
-                                DataCell(Poppins(text: category['name'], size: 14)),
-                                DataCell(Poppins(text: category['isActive'] ? 'Active' : 'Inactive', size: 14)),
-                                DataCell(Poppins(text: 'Menu', size: 14)), // Assuming type is always 'Menu'
-                                DataCell(Poppins(text: '${category['totalProducts']} item', size: 14)),
+                                DataCell(Poppins(
+                                    text: (index + 1 + _currentPage * 10)
+                                        .toString(),
+                                    size: 14)),
+                                DataCell(
+                                    Poppins(text: category['name'], size: 14)),
+                                DataCell(Poppins(
+                                    text: category['isActive']
+                                        ? 'Active'
+                                        : 'Inactive',
+                                    size: 14)),
+                                DataCell(Poppins(
+                                    text: 'Menu',
+                                    size:
+                                        14)), // Assuming type is always 'Menu'
+                                DataCell(Poppins(
+                                    text: '${category['totalProducts']} item',
+                                    size: 14)),
                                 DataCell(
                                   Row(
                                     children: [
                                       IconButton(
-                                        icon: Icon(Icons.visibility),
-                                        onPressed: () {},
-                                      ),
-                                      IconButton(
                                         icon: Icon(Icons.edit),
-                                        onPressed: () {},
+                                        onPressed: () => _showAddCategoryDialog(
+                                            category['id'],
+                                            category['name'],
+                                            category['isActive']),
                                       ),
                                       IconButton(
-                                        icon: Icon(Icons.delete),
-                                        onPressed: () {},
+                                        icon: Icon(Icons.delete,
+                                            color: Colors.red),
+                                        onPressed: () {
+                                          _popupDeleteProductCategory(category);
+                                        },
                                       ),
                                     ],
                                   ),
@@ -286,7 +426,9 @@ class _MsProductCategoryTabState extends State<MsProductCategoryTab> {
                     child: Text('Previous'),
                     style: ButtonStyle(
                       backgroundColor: MaterialStateProperty.resolveWith(
-                        (states) => _currentPage == 0 ? Colors.grey.shade50 : Colors.blue,
+                        (states) => _currentPage == 0
+                            ? Colors.grey.shade50
+                            : Colors.blue,
                       ),
                     ),
                   ),
